d8ca668c3f2b4c064c335be4d90df3d99dd0575d
Eric Nielsen <eniel662@mtroyal.ca>
2022-12-14 18:25:38 -0700

    Minimal styling added, city data retrieval works.

--- script.js
+++ script.js
@@ -1,15 +1,28 @@
 // SCRIPT tag is in the head of HTML, so DOM elements cannot be retrieved without waiting for them to load
-document.addEventListener("DOMContentLoaded", () => {
-    // Button event listener
-    const cityInputBtn = document.querySelector("#city-input-btn");
-    cityInputBtn.addEventListener("click", handleCityData);
-    // Retrieve city data -> retrieve weather data
-    const cityInput = document.querySelector("#city-input").value;
-
-    async function handleCityData(){
-        const userCity = this.value; 
-        const endpoint =  "https://api.npoint.io/51175e4a65b8e108e5ad";
-        const dataFetch = await fetch(endpoint).then(response.json());
-
+async function fetchCityData(){
+    return (await fetch("https://api.npoint.io/51175e4a65b8e108e5ad")).json();
+}
+document.addEventListener("DOMContentLoaded", async () => {
+    // Retrieves the data to be presented for every city
+    let cityData = JSON.parse(localStorage.getItem('cityData')) || "";
+    if (cityData === ""){
+        try{
+            cityData = await fetchCityData(); // Retrieves the 
+            const songsStringified = JSON.stringify(cityData);
+            localStorage.setItem('cityData', songsStringified);
+        } 
+        catch(error){
+            console.log(error);
+        }
     }
+    console.log(cityData[100]);
+    const NUM_CITIES = cityData.length;
+    // Button event listener
+    const cityInputBtn = document.querySelector("#city-submit-btn");
+    cityInputBtn.addEventListener("click", console.log("I've been clicked"));
+    // Retrieve city data -> retrieve weather data
+    const cityInput = document.querySelector("#city-input");
+    cityInput.placeholder = cityData[Math.floor(Math.random() * NUM_CITIES)].city; // Random placeholder for a city 
+
+    function load
 });
